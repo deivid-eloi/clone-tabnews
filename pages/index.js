989d54e7b0f99f886a0de9ee7f5f1efb28cdf397
Deivid Eloi Barbosa <84548220+deivid-eloi@users.noreply.github.com>
2026-03-13 18:18:51 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Olha só amor, ele está vivo. VIVOOOOOOO, o site funcionaaaaaa. Muahahaha. Te amo Emylly!</h1>
+  return (
+    <h1>
+      Olha só amor, ele está vivo. VIVOOOOOOO, o site funcionaaaaaa. Muahahaha.
+      Te amo Emylly!
+    </h1>
+  );
 }
 
 export default Home;
